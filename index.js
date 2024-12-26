134dddae8f4b8023e9ed4d560b967a58fae7acd3
Rokon Khan <rokonjust@gmail.com>
2024-12-26 22:17:17 +0600

    The PATCH Method Added

--- index.js
+++ index.js
@@ -71,6 +71,45 @@ async function run() {
       res.send(result);
     });
 
+    // Get takeassignment by daynmic ID
+    app.get("/takeassignment/:id", async (req, res) => {
+      const { id } = req.params;
+      try {
+        const assignment = await submitionAssignmnet.findOne({
+          _id: new ObjectId(id),
+        });
+        if (!assignment) {
+          return res.status(404).send({ message: "Assignment not found" });
+        }
+        res.send(assignment);
+      } catch (error) {
+        console.error("Error fetching assignment:", error);
+        res.status(500).send({ message: "Error fetching assignment" });
+      }
+    });
+
+    // The Patch Method of the code
+    app.patch("/takeassignment/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+
+      try {
+        const result = await submitionAssignmnet.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updatedData }
+        );
+
+        if (result.matchedCount === 0) {
+          return res.status(404).json({ message: "Assignment not found" });
+        }
+
+        res.status(200).json({ message: "Assignment updated successfully" });
+      } catch (error) {
+        console.error("Error updating assignment:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+
     // Delete Assignment
     app.delete("/allassignmnets/:id", async (req, res) => {
       const id = req.params.id;
